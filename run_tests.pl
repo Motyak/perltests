2425a84466965bc36be770255240f009980e7fc6
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-01-22 20:24:36 +0100

    refactored run tests

--- run_tests.pl
+++ run_tests.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-
 use File::Basename;
 use Cwd 'abs_path';
 if(!defined($INCLUDE_ONCE))
@@ -8,10 +7,58 @@ if(!defined($INCLUDE_ONCE))
     require $INCLUDE_ONCE_FILENAME;
 }
 
-my $CIPHER_TESTS_FILENAME = abs_path(dirname(__FILE__)) . '/cipher_tests.pl';
-require $CIPHER_TESTS_FILENAME;
+my $CIPHER_FILENAME = abs_path(dirname(__FILE__)) . '/cipher.pl';
+require $CIPHER_FILENAME;
 
-cipher_tests::corpus1();
-cipher_tests::echobonjour();
-cipher_tests::multiplelines();
-cipher_tests::multiplelines2();
+package tests;
+
+sub are_equals
+{
+    my ($a, $b) = @_;
+    if($a ne $b)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+sub file_to_str
+{
+    my ($filename) = @_;
+    open my $fh, '<:encoding(UTF-8)', $filename or die "Can't open file $!";
+    read $fh, my $str, -s $fh;
+    return $str;
+}
+
+package cipher_tests;
+
+my $FUNCTION_TO_TEST = \&cipher::do_it;
+
+sub corpus
+{
+    # récupérer tous les noms de fichiers à tester dans le répertoire corpus #
+    opendir my $dir, './corpus/' or die "Cannot open directory: $!";
+    my @corpus = grep{/\.*(?<!_out)\.txt/g} readdir $dir;
+    closedir $dir;
+
+    # faire les tests #
+    foreach(@corpus)
+    {
+        $_ =~ s/(\.*).txt/\1/g;
+        
+        my $input = tests::file_to_str('./corpus/' . $_ . '.txt');
+        my $expecting = tests::file_to_str('./corpus/' . $_ . '_out.txt');
+        my $output = $FUNCTION_TO_TEST->($input);
+
+        if(!tests::are_equals($output, $expecting))
+        {
+            warn "'$_' failed \n\n input='" . $input 
+                    . "' \n\n expecting='" . $expecting
+                    . "' \n\n output='" . $output . "'\n";
+            return 0;
+        }
+    }
+    return 1;
+}
+
+cipher_tests::corpus();
