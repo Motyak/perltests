dca956dfafca3f68c76c1ea6332bf4fe9f37b56f
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-01-23 01:28:00 +0100

    alt ver added

--- main.pl
+++ main.pl
@@ -10,11 +10,38 @@ if(!defined($INCLUDE_ONCE))
 my $CIPHER_FILENAME = abs_path(dirname(__FILE__)) . '/cipher.pl';
 require $CIPHER_FILENAME;
 
-
-my @input;
-while (<STDIN>)
+sub main
 {
-    push @input, $_;
+    my @input;
+    while (<STDIN>)
+    {
+        push @input, $_;
+    }
+    my $joined_input = join('', @input);
+    print(cipher::do_it($joined_input));
 }
-my $joined_input = join('', @input);
-print(cipher::do_it($joined_input));
+
+my $STDIN_ENDING_TOKEN = "cestleclate\n";
+sub main_alt
+{
+    my @input;
+    while (<STDIN>)
+    {
+        last if $_ eq $STDIN_ENDING_TOKEN;
+        push @input, $_;
+    }
+    my $joined_input = join('', @input);
+    print(cipher::do_it($joined_input));
+}
+
+# version pour serveur
+if(@ARGV)
+{
+    while(1)
+    {
+        main_alt();
+    }
+}
+
+# version simple
+main();
